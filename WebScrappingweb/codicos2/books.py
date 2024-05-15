281bf7f60e1fbe76270b8c5ee96d481c6b3d5c15
Roginie <164258862+Roginie@users.noreply.github.com>
2024-05-14 21:56:18 -0300

    Bill
    bookscrap

--- WebScrappingweb/codicos2/books.py
+++ WebScrappingweb/codicos2/books.py
@@ -1,4 +1,3 @@
-#nos queremos pegar todas os linques e em seguida gera uma data set
 import time
 import pandas as pd
 from selenium import webdriver
@@ -9,7 +8,7 @@ service = Service()
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=service, options=options)
 
-url = "https://books.toscrape.com/index.html"
+url = "https://books.toscrape.com/"
 
 driver.get(url)
 time.sleep(2)
@@ -18,7 +17,7 @@ links = driver.find_elements(By.TAG_NAME, value="a")
 print(links)
 print(len(links))
 
-#Os livros comesa no 54 e trermina em 92 de 2 em 2
+#Os livros começam no 54 e terminam no 92
 
 x = driver.find_elements(By.TAG_NAME, value="a")[54].text
 print(x)
@@ -28,13 +27,33 @@ print(y)
 
 print(driver.find_elements(By.TAG_NAME, value="a")[54:94:2])
 
-elementostitulos = driver.find_elements(By.TAG_NAME, value="a")[54:94:2]
+elementostitulo = driver.find_elements(By.TAG_NAME, value="a")[54:94:2]
 
-listatitulos = [title.get_attribute("title") for title in elementostitulos]
+listatitulos = [title.get_attribute("title") for title in elementostitulo]
 print(listatitulos)
 
-elementostitulos[1].click()
+elementostitulo[1].click()
 time.sleep(2)
 stok = driver.find_element(By.CLASS_NAME, value="instock").text
 time.sleep(2)
 print(stok)
+estoque = int(stok.replace("In stock (","").replace(" available)", ""))
+print(estoque)
+driver.back()
+
+listaestoque = []
+
+for titulo in elementostitulo:
+    titulo.click()
+    time.sleep(1)
+    quantidade = int( driver.find_element(By.CLASS_NAME, value="instock").text.replace("In stock (","").replace(" available)", ""))
+    listaestoque.append(quantidade)
+    driver.back()
+    time.sleep(1)
+
+print(listaestoque)
+
+data ={"titulo": listatitulos, "Estoque": listaestoque}
+print(pd.DataFrame(data))
+dados = pd.DataFrame(data)
+#@dados.to_excel("dados.xlsx")
